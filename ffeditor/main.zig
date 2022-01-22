d4980539c935a53a84d9ff3aa331ad05d1827d12
Tien Dung <dungtn@gmail.com>
2022-01-22 12:00:10 +0700

    refine

--- ffeditor/main.zig
+++ ffeditor/main.zig
@@ -3,4 +3,12 @@ const fmt = std.fmt;
 const os = std.os;
 const c = @import("c_imports.zig").c;
 
-pub fn main() void {}
+pub fn main() !void {
+    const stdin_file = std.io.getStdIn();
+    var keybuf: [32]u8 = undefined;
+
+    var num_chars_read: usize = 0;
+    while (num_chars_read == 0) {
+        num_chars_read = try stdin_file.read(keybuf[0..1]);
+    }
+}
